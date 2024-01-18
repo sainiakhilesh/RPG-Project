68186cad2484062f8a6784a9c0344cffc9b49356
sainiakhilesh <akhilesh.saini7@gmail.com>
2024-01-19 03:50:34 +0530

    Add files via upload

--- Scripts/Mover.cs
+++ Scripts/Mover.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,8 +8,24 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] private Transform target;
 
+
     void Update()
     {
-        GetComponent<NavMeshAgent>().destination = target.position;
+        if(Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if(hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
     }
 }
