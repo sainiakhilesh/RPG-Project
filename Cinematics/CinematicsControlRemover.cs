e4df0ff176cfc38153a5e39c6d882d21db4a382f
sainiakhilesh <akhilesh.saini7@gmail.com>
2024-02-10 14:11:22 +0530

    Add files via upload

---
+++ Cinematics/CinematicsControlRemover.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Cinematics
+{
+    public class CinematicsControlRemover : MonoBehaviour
+    {
+
+    }
+
+}
